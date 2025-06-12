dba203c5498a9a484a46dfaad81bc37a67f05091
GhostRider <fewbit.network@gmail.com>
2025-06-12 23:58:11 +0200

    Fix walletnotify string replacement: manual %s replacement without boost::replace_all

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1184,13 +1184,7 @@ bool CWallet::AddToWallet(const CWalletTx& wtxIn, bool fFlushOnClose, bool resca
     std::string strCmd = gArgs.GetArg("-walletnotify", "");
 
     if (!strCmd.empty()) {
-        std::string hexStr = wtxIn.GetHash().GetHex();
-size_t pos = 0;
-while ((pos = strCmd.find("%s", pos)) != std::string::npos) {
-    strCmd.replace(pos, 2, hexStr);
-    pos += hexStr.length();
-}
-
+        boost::replace_all(strCmd, "%s", wtxIn.GetHash().GetHex());
         std::thread t(runCommand, strCmd);
         t.detach(); // thread runs free
     }
