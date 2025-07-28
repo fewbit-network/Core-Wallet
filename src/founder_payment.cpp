c2a5255713a5ae087572f7aee8aea9bc09c1d899
GhostRider <fewbit.network@gmail.com>
2025-07-28 19:00:08 +0200

    Update founder_payment.cpp

--- src/founder_payment.cpp
+++ src/founder_payment.cpp
@@ -1,5 +1,17 @@
+/*
+ * Copyright (c) 2018 The Pigeon Core developers
+ * Distributed under the MIT software license, see the accompanying
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.
+ *
+ * FounderPayment.cpp
+ *
+ *  Created on: Jun 24, 2018
+ *      Author: Tri Nguyen
+ */
+
 #include <founder_payment.h>
 #include <rpc/server.h>
+
 #include <boost/foreach.hpp>
 #include <chainparams.h>
 #include <key_io.h>
@@ -21,42 +33,38 @@ CAmount FounderPayment::getFounderPaymentAmount(int blockHeight, CAmount blockRe
 
 void FounderPayment::FillFounderPayment(CMutableTransaction& txNew, int nBlockHeight, CAmount blockReward, CTxOut& txoutFounderRet)
 {
+    // make sure it's not filled yet
     CAmount founderPayment = getFounderPaymentAmount(nBlockHeight, blockReward);
+    //	if(founderPayment == 0) {
+    //	    LogPrintf("FounderPayment::FillFounderPayment -- Founder payment has not started\n");
+    //	    return;
+    //
+    //	}
     txoutFounderRet = CTxOut();
+    // fill payee with the foundFounderRewardStrcutureFounderRewardStrcutureer address
+    CTxDestination founderAddr = DecodeDestination(founderAddress);
+    if (!IsValidDestination(founderAddr))
+        throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, strprintf("Invalid FewBit Founder Address: %s", founderAddress.c_str()));
+    CScript payee = GetScriptForDestination(founderAddr);
+    // GET FOUNDER PAYMENT VARIABLES SETUP
 
-    if (founderPayment > 0) {
-        CTxDestination founderAddr = DecodeDestination(GetActiveFounderAddress(nBlockHeight));
-        if (!IsValidDestination(founderAddr))
-            throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, strprintf("Invalid FewBit Founder Address: %s", GetActiveFounderAddress(nBlockHeight).c_str()));
-
-        CScript payee = GetScriptForDestination(founderAddr);
-        txNew.vout[0].nValue -= founderPayment;
-        txoutFounderRet = CTxOut(founderPayment, payee);
-        txNew.vout.push_back(txoutFounderRet);
-
-        LogPrintf("FounderPayment::FillFounderPayment -- Founder payment %lld to %s at block %d\n", founderPayment, GetActiveFounderAddress(nBlockHeight).c_str(), nBlockHeight);
-    }
+    // split reward between miner ...
+    txNew.vout[0].nValue -= founderPayment;
+    txoutFounderRet = CTxOut(founderPayment, payee);
+    txNew.vout.push_back(txoutFounderRet);
+    LogPrintf("FounderPayment::FillFounderPayment -- Founder payment %lld to %s\n", founderPayment, founderAddress.c_str());
 }
 
 bool FounderPayment::IsBlockPayeeValid(const CTransaction& txNew, const int height, const CAmount blockReward)
 {
-    CScript payee = GetScriptForDestination(DecodeDestination(GetActiveFounderAddress(height)));
+    // fill payee with the founder address
+    CScript payee = GetScriptForDestination(DecodeDestination(founderAddress));
     const CAmount founderReward = getFounderPaymentAmount(height, blockReward);
-
     BOOST_FOREACH (const CTxOut& out, txNew.vout) {
         if (out.scriptPubKey == payee && out.nValue >= founderReward) {
             return true;
         }
     }
 
-    return founderReward == 0;
-}
-
-std::string GetActiveFounderAddress(int nHeight)
-{
-    if (nHeight >= 300300) {
-        return "FBA69LP6syGpa3zGiiaQmkSUTY2XrtQbov";  // Nuovo founder address dal block 300300
-    } else {
-        return DEFAULT_FOUNDER_ADDRESS;  // Address storico fino al 300299
-    }
+    return false;
 }
